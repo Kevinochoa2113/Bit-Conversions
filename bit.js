3b0dada117ce45831e847e1b6550764c165ed510
Kevin Ochoa Garcia <Kochoa282@west-mec.local>
2020-09-09 17:47:51 -0700

    Added files to repository

---
+++ bit.js
@@ -0,0 +1,16 @@
+let byte = prompt("How many byte are there?");
+let bits = byte*8
+
+let kilobyte = byte/1024;
+let megabyte = kilobyte/1024;
+let gigabyte = megabyte/1024;
+let terabyte = gigabyte/1024
+
+console.log(`bits = ${bits}`);
+console.log(`byte = ${byte}`);
+console.log(`kilobyte = ${kilobyte}`);
+console.log(`megabyte = ${megabyte}`);
+console.log(`gigabyte = ${gigabyte}`);
+console.log(`terabyte = ${terabyte}`);
+
+
